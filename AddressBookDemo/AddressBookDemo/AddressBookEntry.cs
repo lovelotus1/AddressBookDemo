ef0e959a89209ff7d1ebf0225ef64bc41e4f60d1
“Love <opera.prem@gmail.com>
2022-08-13 22:40:07 +0430

    Added UC4_AbilityToDeletePerson

--- AddressBookDemo/AddressBookDemo/AddressBookEntry.cs
+++ AddressBookDemo/AddressBookDemo/AddressBookEntry.cs
@@ -6,157 +6,105 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    public class EditEntry
+    public class AddressBookEntry : IContact
     {
-        public string Firstname;
-        public string Lastname;
-        public string Address;
-        public string City;
-        public string State;
-        public string Postalcode;
-        public string PhoneNumber;
-        public string email;
-        public static List<EditEntry> People = new List<EditEntry>();
-        public static void NewContact()
+        //Creating a contact list
+        List<Contact> contactList;
+        public AddressBookEntry()
         {
-            EditEntry person = new EditEntry();
-            Console.Write("Enter First Name: ");
-            person.Firstname = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            person.Lastname = Console.ReadLine();
-            Console.Write("Enter Address: ");
-            person.Address = Console.ReadLine();
-            Console.Write("Enter City name: ");
-            person.City = Console.ReadLine();
-            Console.Write("Enter State name: ");
-            person.State = Console.ReadLine();
-            Console.Write("Enter the Zip Code: ");
-            person.Postalcode = Console.ReadLine();
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNumber = Console.ReadLine();
-            Console.Write("Enter the Email ID: ");
-            person.email = Console.ReadLine();
-            People.Add(person);
+            contactList = new List<Contact>();
         }
-        public static void AllDetails(EditEntry person)
+        //Method to create contact(UC1) 
+        public void AddContactDetails(string firstName, string lastName, string address, string city, string state, int postalCode, long phoneNumber, string emailId)
         {
-            Console.WriteLine("First Name: " + person.Firstname);
-            Console.WriteLine("Last Name: " + person.Lastname);
-            Console.WriteLine("Address:" + person.Address);
-            Console.WriteLine("City: " + person.City);
-            Console.WriteLine("State: " + person.State);
-            Console.WriteLine("Zip Code: " + person.Postalcode);
-            Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Email ID: " + person.email);
-
+            Contact personDetail = new Contact(firstName, lastName, address, city, state, postalCode, phoneNumber, emailId);
+            contactList.Add(personDetail);
         }
-        public static void ListAllContact()
+        //Method to view contact
+        public void ViewContact()
         {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("People in your Address Book:\n");
-            Console.ResetColor();
-            foreach (var person in People)
+            int count = 1;
+            foreach (var contact in contactList)
             {
-                AllDetails(person);
+                Console.WriteLine("Person Details {0} ------> ", count);
+                Console.WriteLine("First Name : {0} || Last Name : {1}", contact.firstName, contact.lastName);
+                Console.WriteLine("Address : {0} ", contact.address);
+                Console.WriteLine("City Name : {0} || State Name : {1} || PostalCode : {2}", contact.city, contact.state, contact.PostalCode);
+                Console.WriteLine("Phone Number : {0}", contact.phoneNumber);
+                Console.WriteLine("Email Id : {0} ", contact.emailId);
+                Console.ReadLine();
+                count++;
             }
         }
-        public static void Update()
+        //Method to edit contacts(UC3)
+        public void EditContact(string personName)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Press 1 If you want to edit any Contact in Address Book");
-            Console.ResetColor();
-            switch (Console.ReadLine())
+            //Traversing the contact list
+            for (int i = 0; i < contactList.Count; i++)
             {
-                case "1":
-                    Console.WriteLine("Enter the First Name of the Person U want to update");
-                    string firstname = Console.ReadLine();
-                    EditEntry person = People.Find(x => x.Firstname.ToLower() == firstname.ToLower());
-                    if (person == null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("That person U entered is not found\n");
-                        Console.ResetColor();
-                        EditEntry.Update();
-                    }
-                    Console.WriteLine("1)If u want to edit the Firstname");
-                    Console.WriteLine("2)If u want to edit the LastName");
-                    Console.WriteLine("3)If u want to edit the City");
-                    Console.WriteLine("4)If u want to edit the State");
-                    Console.WriteLine("5)If u want to edit the Zip Code");
-                    Console.WriteLine("6)If u want to edit the Phone Number");
-                    Console.WriteLine("7)If u want to edit the Email Id");
-                    int choice = Convert.ToInt32(Console.ReadLine());
+                if (contactList[i].firstName == personName)
+                {
+                    Console.WriteLine("\nChoose What You Want To Edit" +
+                                "\n1.First Name" +
+                                "\n2.Last Name" +
+                                "\n3.Address" +
+                                "\n4.City" +
+                                "\n5.State" +
+                                "\n6.Area Postal Code" +
+                                "\n7.Phone Number" +
+                                "\n8.Email Id"
+                                );
+                    int choice = int.Parse(Console.ReadLine());
                     switch (choice)
                     {
                         case 1:
-                            Console.WriteLine("Enter New Firstname: ");
-                            person.Firstname = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("First Name: " + person.Firstname);
+                            Console.Write("Enter A New Entry For First Name : ");
+                            contactList[i].firstName = Console.ReadLine();
                             break;
-
                         case 2:
-                            Console.WriteLine("Enter New LastName: ");
-                            person.Lastname = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("Last Name: " + person.Lastname);
+                            Console.Write("Enter A New Entry For Last Name : ");
+                            contactList[i].lastName = Console.ReadLine();
                             break;
-
                         case 3:
-                            Console.WriteLine("Enter New Address:");
-                            person.Address = Console.ReadLine();
-                            People.Add(person);
+                            Console.Write("Enter A New Entry For Address : ");
+                            contactList[i].address = Console.ReadLine();
                             break;
-
-
                         case 4:
-                            Console.WriteLine("Enter New City: ");
-                            person.City = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("City: " + person.City);
+                            Console.Write("Enter A New Entry For New City : ");
+                            contactList[i].city = Console.ReadLine();
                             break;
-
                         case 5:
-                            Console.WriteLine("Enter New State: ");
-                            person.State = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("State Name: " + person.State);
+                            Console.Write("Enter A New Entry For State : ");
+                            contactList[i].state = Console.ReadLine();
                             break;
-
                         case 6:
-                            Console.WriteLine("Enter New ZipCode: ");
-                            person.Postalcode = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("ZipCode: " + person.Postalcode);
+                            Console.Write("Enter A New Entry For Postal Code : ");
+                            contactList[i].PostalCode = int.Parse(Console.ReadLine());
                             break;
-
                         case 7:
-                            Console.WriteLine("Enter New PhoneNumber: ");
-                            person.PhoneNumber = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("PhoneNumber: " + person.PhoneNumber);
+                            Console.Write("Enter A New Entry For Phone Number : ");
+                            contactList[i].phoneNumber = long.Parse(Console.ReadLine());
                             break;
-
                         case 8:
-                            Console.WriteLine("Enter New Email ID: ");
-                            person.email = Console.ReadLine();
-                            People.Add(person);
-                            Console.WriteLine("Email Id: " + person.email);
-                            break;
-
-                        default:
-                            Console.WriteLine("Invalid Choice");
+                            Console.Write("Enter A New Entry For Email Id : ");
+                            contactList[i].emailId = Console.ReadLine();
                             break;
                     }
-                    ListAllContact();
-                    break;
-                default:
-                    Console.WriteLine("Thanku!!!");
-                    break;
+                }
             }
+        }
+        //Method to delete contact details using first name(UC4)
+        public void DeleteContact(string personName)
+        {
+            for (int i = 0; i < contactList.Count; i++)
+            {
+                if (contactList[i].firstName == personName)
+                {
+                    Console.WriteLine("Record Of {0} Deleted Successfully", contactList[i].firstName);
+                    contactList.RemoveAt(i);
+                }
+            }
+
         }
     }
 }
-        
-    
-
--- AddressBookDemo/AddressBookDemo/Contact.cs
+++ AddressBookDemo/AddressBookDemo/Contact.cs
@@ -3,54 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace AddressBook
 {
-    public class CreateNewContact
+    public class Contact
     {
-        public string Firstname;
-        public string Lastname;
-        public string City;
-        public string State;
-        public string Zipcode;
-        public string PhoneNumber;
-        public string email;
-        public void Show()
+        // Declaring contact details properties
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int PostalCode;
+        public long phoneNumber;
+        public string emailId;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int PostalCode, long phoneNumber, string emailId)
         {
-            CreateNewContact cc = new CreateNewContact();
-
-            Console.Write("Enter First Name: ");
-            cc.Firstname = Console.ReadLine();
-
-            Console.Write("Enter Last Name: ");
-            cc.Lastname = Console.ReadLine();
-
-            Console.Write("Enter City name: ");
-            cc.City = Console.ReadLine();
-
-            Console.Write("Enter State name: ");
-            cc.State = Console.ReadLine();
-
-            Console.Write("Enter the Zip Code: ");
-            cc.Zipcode = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            cc.PhoneNumber = Console.ReadLine();
-
-            Console.Write("Enter the Email ID: ");
-            cc.email = Console.ReadLine();
-
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("\nDetails of the Person are given below: ");
-            Console.ResetColor();
-            Console.WriteLine("\nFirst Name: " + cc.Firstname);
-            Console.WriteLine("\nLast Name: " + cc.Lastname);
-            Console.WriteLine("\nCity: " + cc.City);
-            Console.WriteLine("\nState: " + cc.State);
-            Console.WriteLine("\nZip Code: " + cc.Zipcode);
-            Console.WriteLine("\nPhone Number: " + cc.PhoneNumber);
-            Console.WriteLine("\nEmail ID: " + cc.email);
-            Console.WriteLine("-------------------------------------------");
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.PostalCode = PostalCode;
+            this.phoneNumber = phoneNumber;
+            this.emailId = emailId;
         }
     }
 }
