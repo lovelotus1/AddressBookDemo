cfcdc92da5923cad38d0114bd1952b45ab5c807a
“Love <opera.prem@gmail.com>
2022-08-13 22:19:15 +0430

    Added UC4_DeleteContact

--- AddressBookDemo/AddressBookDemo/Program.cs
+++ AddressBookDemo/AddressBookDemo/Program.cs
@@ -1,17 +1,22 @@
 ﻿using AddressBook;
 
 Console.WriteLine("==========Welcome To Address Book Program==========");
-Console.Write("Select Number:\n1)AddContacts\n2)EditContact\n");
+Console.Write("Select Number:\n1)AddContacts\n2)EditContact\n3)DeleteContact\n");
 int option = Convert.ToInt32(Console.ReadLine());
 switch (option)
 {
     case 1:
-        new Contact();
+        new CreateNewContact().Show();
         break;
     case 2:
         EditEntry.NewContact();
         EditEntry.ListAllContact();
         EditEntry.Update();
+        break;
+    case 3:
+        DeleteContact.NewContact();
+        DeleteContact.ListAllContacts();
+        DeleteContact.Delete();
         break;
     default:
         Console.Write("Please Select Correct Number");
